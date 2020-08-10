81afb74b6f93303b535eb7d2a32f1526bb44160d
rogeriodejesusbarros <69017763+rogeriodejesusbarros@users.noreply.github.com>
2020-08-10 16:59:45 -0300

    Correção do endereço API

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL="https://java-api-spring-bot.herokuapp.com/validacpf?cpf="
+const URL="https://java-api-https://valida-cpf-bakc-rogerio.herokuapp.com/-bot.herokuapp.com/validacpf?cpf="
 function validar(){
 
 	var txtCPF = document.getElementById("txtCpf").value;
